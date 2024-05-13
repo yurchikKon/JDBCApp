386fa4d1cdc2cb9ec4b9a4cf5c6f515b9cf23878
yurchikKon <157982309+yurchikKon@users.noreply.github.com>
2024-05-13 19:30:34 +0300

    Update DepartmentRepositoryImpl.java

--- src/main/java/com/intensive/jdbcPr/repository/impl/DepartmentRepositoryImpl.java
+++ src/main/java/com/intensive/jdbcPr/repository/impl/DepartmentRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Repository
 public class DepartmentRepositoryImpl implements DepartmentRepository {
 
     private static final String UPDATE_QUERY = "update department\n" +
